b90539f4e007f3df46051fe4f1d782da084b8c57
brownmj93 <brownmj93@gmail.com>
2019-11-12 23:38:50 -0500

    changed port number from 8080 to 3000

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
